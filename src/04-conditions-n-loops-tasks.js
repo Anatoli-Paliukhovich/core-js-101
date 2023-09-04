5cbf5df74dabe41ac60e33d1fed4cd241d0d179f
“Anatoli-Paliukhovich” <humanities5050@gmail.com>
2023-09-04 23:02:54 +0300

    feat: solve some more condition tasks

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -297,8 +297,9 @@ function isCreditCardNumber(/* ccn */) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  const numSum = num.toString().split('').reduce((prevNum, number) => +prevNum + +number);
+  return numSum.toString().split('').reduce((prevNum, number) => +prevNum + +number);
 }
 
 
